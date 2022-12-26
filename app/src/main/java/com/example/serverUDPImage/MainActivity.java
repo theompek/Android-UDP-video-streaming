c761074500d8f918521d3d86c1ce81e9831ffbb2
Fanis <theompek>
2022-12-26 19:35:43 +0200

    .

--- app/src/main/java/com/example/serverUDPImage/MainActivity.java
+++ app/src/main/java/com/example/serverUDPImage/MainActivity.java
@@ -45,6 +45,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
